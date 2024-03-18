aaae9d8f962d982b0a89993183cb9765b1089081
david <dprydereid@gmail.com>
2024-03-18 12:21:24 +0000

    ui(feature): empty vuex state machine for chat widget added

---
+++ ui/default/src/modules/chatStore.js
@@ -0,0 +1,26 @@
+//Store for chat features
+
+
+const chatStore = {
+    state: () => ({
+       
+       }),
+       mutations:{
+         
+         
+
+       },
+       actions:{
+         
+
+
+       },
+       getters:{
+         
+         
+         
+       },  
+  
+  }
+
+  export default chatStore;
