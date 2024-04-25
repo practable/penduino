95be15a146a6c477c2fd04b824dad534f7eba7c4
david <dprydereid@gmail.com>
2024-04-25 16:07:00 +0100

    ui (fix): TableOutput.vue a space was removed after row class

--- ui/default/src/components/TableOutput.vue
+++ ui/default/src/components/TableOutput.vue
@@ -1,6 +1,6 @@
 <template>
 <div class='container-fluid m-2 practable-component table'>
-    <div class="row ">
+    <div class="row">
     <!-- <input type="text" id="search" v-on:keyup="search" v-model="search_field" placeholder="Search the table..."> -->
 
         <table v-for="table in tableData">
